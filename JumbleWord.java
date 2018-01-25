c82cb74424e12ba4e92adc6347ca8f981664e46b
Sahil Verma <verma-rishu294@users.noreply.github.com>
2018-01-25 13:14:51 +0530

    Update JumbleWord.java

--- JumbleWord.java
+++ JumbleWord.java
@@ -1,5 +1,26 @@
 /**
- * Created by somebody on 1/25/2018.
+ * Created by Sahil Verma on 1/25/2018.
+ * **********************************************************
+ * Problem statement:
+ * ------------------
+ * User will give the input like : "2,CAT,ROB,BAT,CAP"
+ * first digit : signifies we have to create 2 words by using pairs of two words
+ * futhure arguments are the input words.
+ * 
+ * For example:
+ * "2,CAT,ROB,BAT,CAP"
+ * first word will be created from using CAT and ROB
+ * second word will be created from using BAT and CAP
+ * 
+ * How to create first word:
+ * compare first char of CAT -> C with the first char of BAT -> B
+ * which ever is lexigally small, will be the first char of the resultant word.. and so on.....
+ * 
+ * First and Second Output Word will be: CAROBT and BACAPT
+ * 
+ * This problem is vert easy, try to solve it in 15 minutes of time.
+ * 
+ *
  */
 public class JumbleWord {
 
